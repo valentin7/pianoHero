44c1f21b7e383b7467b517e8e28fcd4739d7fb69
Nicole Byer <nicole_byer@brown.edu>
2015-04-02 21:21:00 -0400

    Added handlers to the Main class

--- src/main/java/edu/brown/cs/pianoHero/Main.java
+++ src/main/java/edu/brown/cs/pianoHero/Main.java
@@ -1,7 +1,161 @@
 package edu.brown.cs.pianoHero;
 
-public class Main {
-  public static void main(String[] args) {
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import spark.ExceptionHandler;
+import spark.ModelAndView;
+import spark.QueryParamsMap;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+import spark.Spark;
+import spark.SparkBase;
+import spark.TemplateViewRoute;
+import spark.template.freemarker.FreeMarkerEngine;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.gson.Gson;
+
+import freemarker.template.Configuration;
+
+public class Main {
+
+  private static class ExceptionPrinter implements ExceptionHandler {
+
+    @Override
+    public void
+    handle(final Exception e, final Request req, final Response res) {
+      res.status(STATUS);
+      final StringWriter stacktrace = new StringWriter();
+      try (PrintWriter pw = new PrintWriter(stacktrace)) {
+        pw.println("<pre>");
+        e.printStackTrace(pw);
+        pw.println("</pre>");
+      }
+      res.body(stacktrace.toString());
+    }
+  }
+
+  private static class HighScoresHandler implements Route {
+    @Override
+    public Object handle(final Request req, final Response res) {
+      // TODO get all the SongScores from the database and store them in scores
+      // so they correspond with their song ID
+      final Map<String, ArrayList<SongScore>> scores =
+          new HashMap<String, ArrayList<SongScore>>();
+
+      return GSON.toJson(scores);
+    }
+  }
+
+  private static class HighScoresView implements TemplateViewRoute {
+    @Override
+    public ModelAndView handle(Request req, Response res) {
+      final Map<String, Object> variables =
+          ImmutableMap.of("title", "PianoHero: High Scores");
+      return new ModelAndView(variables, "highScores.ftl");
+    }
+  }
+
+  private static class MainMenuSongsHandler implements Route {
+    @Override
+    public Object handle(final Request req, final Response res) {
+      // TODO get all the Songs from the database and store them in songs
+      final List<Song> songs = new ArrayList<Song>();
+
+      return GSON.toJson(songs);
+    }
+  }
+
+  private static class MainMenuView implements TemplateViewRoute {
+    @Override
+    public ModelAndView handle(Request req, Response res) {
+      final Map<String, Object> variables =
+          ImmutableMap.of("title", "PianoHero");
+      return new ModelAndView(variables, "mainMenu.ftl");
+    }
+  }
+
+  private static class PlaySongView implements TemplateViewRoute {
+    @Override
+    public ModelAndView handle(Request req, Response res) {
+      final Map<String, Object> variables =
+          ImmutableMap.of("title", "PianoHero: Play Song");
+      return new ModelAndView(variables, "playSong.ftl");
+    }
+  }
+
+  private static class SongFactoryHandler implements Route {
+    @Override
+    public Object handle(final Request req, final Response res) {
+      final QueryParamsMap qm = req.queryMap();
+      final String title = qm.value("title");
+      // TODO figure out best way to store mp3/ image files -> should
+      // back end or front end store them and how?
+      final String mp3Path = qm.value("mp3Path");
+      final String imagePath = qm.value("imagePath");
+      // TODO figure out if this works? should we be storing songs as 2D arrays
+      // or as HashMaps?
+      final Map keyStrokes = GSON.fromJson(qm.value("keyStrokes"), Map.class);
+
+      // TODO store the song data in the database
+
+      return null;
+    }
+  }
+
+  private static class SongFactoryView implements TemplateViewRoute {
+    @Override
+    public ModelAndView handle(Request req, Response res) {
+      final Map<String, Object> variables =
+          ImmutableMap.of("title", "PianoHero: Song Factory");
+      return new ModelAndView(variables, "songFactory.ftl");
+    }
+  }
+
+  private static final Gson GSON = new Gson();
+
+  private static final int STATUS = 500;
+
+  private static FreeMarkerEngine createEngine() {
+    final Configuration config = new Configuration();
+    final File templates =
+        new File("src/main/resources/spark/template/freemarker");
+    try {
+      config.setDirectoryForTemplateLoading(templates);
+    } catch (final IOException ioe) {
+      System.out.printf("ERROR: Unable use %s for template loading.%n",
+          templates);
+      System.exit(1);
+    }
+    return new FreeMarkerEngine(config);
+  }
+
+  public static void main(String[] args) {
+    runSparkServer();
+  }
+
+  private static void runSparkServer() {
+    SparkBase.externalStaticFileLocation("src/main/resources/static");
+    Spark.exception(Exception.class, new ExceptionPrinter());
+
+    final FreeMarkerEngine freeMarker = createEngine();
+
+    // Setup Spark Routes
+    Spark.get("/", new MainMenuView(), freeMarker);
+    Spark.get("/highscores", new HighScoresView(), freeMarker);
+    Spark.get("/playsong", new PlaySongView(), freeMarker);
+    Spark.get("/songfactory", new SongFactoryView(), freeMarker);
+
+    Spark.get("/getsongs", new MainMenuSongsHandler());
+    Spark.get("/gethighscores", new HighScoresHandler());
+    Spark.post("/storesong", new SongFactoryHandler());
   }
 }
