7919d1cf79b57e66070ee7b5a8a99ed69b72bb15
Valentin Perez <valpe70@gmail.com>
2015-04-16 13:52:41 -0400

    improved random keys thing

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,26 +1,34 @@
+
+
 $(document).ready(function() {
 console.log("PIANO HERO");
-
-
 
 });
 
 
 function createSong() {
 
-  songLength = Math.round(Math.random() * 300 + 20);
+  songLength = Math.round(Math.random() * 100 + 32);
   maxKeys = 8;
   console.log(songLength);
   var randomKeyStrokes = new Array(songLength);
 
   for (var i = 0; i < songLength; i ++) {
-    startingIndex = Math.round(Math.random() * 8);
-    skipStep = Math.round(Math.random() * maxKeys);
+    //startingIndex = Math.round(Math.random() * 8);
+    skipStep = Math.floor(Math.random() * maxKeys);
 
-    randomKeyStrokes[i] = new Array();
-    for (var strokeIndex = 0; strokeIndex < maxKeys; strokeIndex+=skipStep) {
-      randomKeyStrokes[i][strokeIndex] = true;
+    console.log("got here");
+    randomKeyStrokes[i] = new Array(maxKeys);
+    randomKeyStrokes[i][skipStep] = true;
+
+    for (var j = 1; j < maxKeys; j++) {
+      if (j != skipStep){
+        randomKeyStrokes[i][j] = false;
+      }
     }
+    /*for (var strokeIndex = 0; strokeIndex < maxKeys; strokeIndex+=skipStep) {
+      randomKeyStrokes[i][strokeIndex] = true;
+    }*/
   }
 
   console.log("created random song strokes:");
