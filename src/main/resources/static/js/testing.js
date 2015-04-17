33130fd7bcd7f3de1d4029b888606ae0008790a6
Valentin Perez <valpe70@gmail.com>
2015-04-16 21:46:14 -0400

    testing files

---
+++ src/main/resources/static/js/testing.js
@@ -0,0 +1,36 @@
+
+
+$(document).ready(function() {
+console.log("PIANO HERO");
+
+});
+
+
+function createSong() {
+
+  songLength = Math.round(Math.random() * 100 + 32);
+  maxKeys = 8;
+  console.log(songLength);
+  var randomKeyStrokes = new Array(songLength);
+
+  for (var i = 0; i < songLength; i ++) {
+    //startingIndex = Math.round(Math.random() * 8);
+    skipStep = Math.floor(Math.random() * maxKeys);
+
+    console.log("got here");
+    randomKeyStrokes[i] = new Array(maxKeys);
+    randomKeyStrokes[i][skipStep] = true;
+
+    for (var j = 0; j < maxKeys; j++) {
+      if (j != skipStep){
+        randomKeyStrokes[i][j] = false;
+      }
+    }
+    /*for (var strokeIndex = startingIndex; strokeIndex < maxKeys; strokeIndex+=skipStep) {
+      randomKeyStrokes[i][strokeIndex] = true;
+    }*/
+  }
+
+  console.log("created random song strokes:");
+  console.log(randomKeyStrokes);
+}
